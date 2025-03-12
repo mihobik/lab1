7568c73139e08e739b5189bc2ff7b38d4c4b5c43
DayanaVan <19dayanavl06@mail.ru>
2025-03-12 04:52:22 +0000

    изменение кода: трапеция

--- trapezoid.cpp
+++ trapezoid.cpp
@@ -4,29 +4,37 @@
 using namespace std;
 
 int main() {
-    double a, b, h;
+    double a, b, c, d, h;
 
     // Запрашиваем длины оснований и высоту трапеции
-    cout << "Enter lengths of the bases of the trapezoid (a b) and height (h): ";
-    cin >> a >> b >> h;
+    do {
+        cout << "Enter lengths of the bases of the trapezoid (a b) and height (h): ";
+        cin >> a >> b >> h;
+        if (a <= 0 || b <= 0 || h <= 0) {
+            cout << "Lengths of bases and height must be positive numbers. Please try again." << endl;
+        }
+    } while (a <= 0 || b <= 0 || h <= 0);
 
-    // Проверка на положительность оснований и высоты
-    if (a > 0 && b > 0 && h > 0) {
-        // Периметр
-        double perimeter = a + b + 2 * sqrt(h * h + pow((b - a) / 2, 2));
-        cout << "Perimeter: " << perimeter << endl;
+    // Запрашиваем длины боковых сторон
+    do {
+        cout << "Enter lengths of the non-parallel sides (c d): ";
+        cin >> c >> d;
+        if (c <= 0 || d <= 0 || c < h || d < h) {
+            cout << "Lengths of non-parallel sides must be positive numbers. Please try again." << endl;
+        }
+    } while (c <= 0 || d <= 0 || c < h || d < h);
 
-        // Площадь
-        double area = ((a + b) / 2) * h;
-        cout << "Area: " << area << endl;
+    // Периметр
+    double perimeter = a + b + c + d;
+    cout << "Perimeter: " << perimeter << endl;
 
-        // Длина средней линии
-        double midline = (a + b) / 2;
-        cout << "Length of the midline: " << midline << endl;
-    }
-    else {
-        cout << "Lengths of bases and height must be positive numbers." << endl;
-    }
+    // Площадь
+    double area = ((a + b) / 2) * h;
+    cout << "Area: " << area << endl;
+
+    // Длина средней линии
+    double midline = (a + b) / 2;
+    cout << "Length of the midline: " << midline << endl;
 
     return 0;
 }
